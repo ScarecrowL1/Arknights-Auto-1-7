f6cf4934b7ddd68d1bd1f1f9ad04391ee84390bc
Tsukimidai <710939240@qq.com>
2023-03-06 17:45:31 +0800

    识别功能正常，添加了返回位置以及筛选精确识别的条件

--- reconize.py
+++ reconize.py
@@ -1,5 +1,6 @@
 from ctypes import windll, byref, c_ubyte
 from ctypes.wintypes import RECT, HWND
+import cv2
 import numpy as np
 
 GetDC = windll.user32.GetDC
@@ -15,6 +16,7 @@ ReleaseDC = windll.user32.ReleaseDC
 
 # 排除缩放干扰# #
 windll.user32.SetProcessDPIAware()
+
 
 def capture(handle: HWND):
     """窗口客户区截图
@@ -36,9 +38,9 @@ def capture(handle: HWND):
     SelectObject(cdc, bitmap)
     BitBlt(cdc, 0, 0, width, height, dc, 0, 0, SRCCOPY)
     # 截图是BGRA排列，因此总元素个数需要乘以4
-    total_bytes = width*height*4
+    total_bytes = width * height * 4
     buffer = bytearray(total_bytes)
-    byte_array = c_ubyte*total_bytes
+    byte_array = c_ubyte * total_bytes
     GetBitmapBits(bitmap, total_bytes, byte_array.from_buffer(buffer))
     DeleteObject(bitmap)
     DeleteObject(cdc)
@@ -46,9 +48,39 @@ def capture(handle: HWND):
     # 返回截图数据为numpy.ndarray
     return np.frombuffer(buffer, dtype=np.uint8).reshape(height, width, 4)
 
+
+def match(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGRA2GRAY)
+    # 读取图片，并保留Alpha通道
+    template = cv2.imread('./image/opra_comp.png', cv2.IMREAD_UNCHANGED)
+    # 取出Alpha通道
+    alpha = template[:, :, 3]
+    template = cv2.cvtColor(template, cv2.COLOR_BGRA2GRAY)
+    # 模板匹配，将alpha作为mask，TM_CCORR_NORMED方法的计算结果范围为[0, 1]，越接近1越匹配
+    result = cv2.matchTemplate(gray, template, cv2.TM_CCORR_NORMED, mask=alpha)
+    print(cv2.minMaxLoc(result))
+    # 获取结果中最大值和最小值以及他们的坐标
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    if max_val < 0.99:
+        return None
+    top_left = max_loc
+
+    h, w = template.shape[:2]
+    bottom_right = top_left[0] + w, top_left[1] + h
+    cv2.rectangle(image, top_left, bottom_right, (0, 0, 255), 2)
+    cv2.imshow('Match Template', image)
+    cv2.waitKey()
+
+    locx = int((top_left[0] + bottom_right[0]) / 2)
+    locy = int((top_left[1] + bottom_right[1]) / 2)
+    location = [locx, locy]
+    return location
+
+
 if __name__ == "__main__":
-    import cv2
     handle = windll.user32.FindWindowW(None, "明日方舟 - MuMu模拟器")
     image = capture(handle)
-    cv2.imshow("Capture Test", image)
-    cv2.waitKey()
+
+    if match(image) is None:
+        print("未找到")
