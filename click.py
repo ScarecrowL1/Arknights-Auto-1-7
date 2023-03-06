0ae076031058d8d8a4748ef25d7d7dfb7837a9f3
Tsukimidai <710939240@qq.com>
2023-03-06 16:32:54 +0800

    对点击功能进行了debug,完成了点击功能

---
+++ click.py
@@ -0,0 +1,74 @@
+from ctypes import windll, byref
+from ctypes.wintypes import HWND, POINT
+import time
+
+PostMessageW = windll.user32.PostMessageW
+ClientToScreen = windll.user32.ClientToScreen
+
+WM_MOUSEMOVE = 0x0200
+WM_LBUTTONDOWN = 0x0201
+WM_LBUTTONUP = 0x202
+WM_MOUSEWHEEL = 0x020A
+WHEEL_DELTA = 120
+
+
+def move_to(handle: HWND, x: int, y: int):
+    """移动鼠标到坐标（x, y)
+
+    Args:
+        handle (HWND): 窗口句柄
+        x (int): 横坐标
+        y (int): 纵坐标
+    """
+    # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-mousemove
+    wparam = 0
+    lparam = y << 16 | x
+    PostMessageW(handle, WM_MOUSEMOVE, wparam, lparam)
+
+
+def left_down(handle: HWND, x: int, y: int):
+    """在坐标(x, y)按下鼠标左键
+
+    Args:
+        handle (HWND): 窗口句柄
+        x (int): 横坐标
+        y (int): 纵坐标
+    """
+    # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-lbuttondown
+    wparam = 1
+    lparam = y << 16 | x
+    PostMessageW(handle, WM_LBUTTONDOWN, wparam, lparam)
+
+
+def left_up(handle: HWND, x: int, y: int):
+    """在坐标(x, y)放开鼠标左键
+
+    Args:
+        handle (HWND): 窗口句柄
+        x (int): 横坐标
+        y (int): 纵坐标
+    """
+    # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-lbuttonup
+    wparam = 0
+    lparam = y << 16 | x
+    PostMessageW(handle, WM_LBUTTONUP, wparam, lparam)
+
+def click(handle: HWND, x: int, y: int):
+    left_down(handle, x, y)
+    time.sleep(0.1)
+    left_up(handle, x, y)
+
+
+if __name__ == "__main__":
+    # 需要和目标窗口同一权限，游戏窗口通常是管理员权限
+    import sys
+    if not windll.shell32.IsUserAnAdmin():
+        # 不是管理员就提权
+        windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, __file__, None, 1)
+
+    import cv2
+    handle = windll.user32.FindWindowW(None, "明日方舟 - MuMu模拟器")
+    # 点击线路
+    click(handle, 1042, 514)
+    # 滚动线路列表
